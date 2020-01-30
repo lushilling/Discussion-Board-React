2e7b7cc0e3810e1aebc60eac746f99ad1ec82935
lucy <lucy.shilling@baesystems.com>
2020-01-30 15:29:25 +0000

    changing

--- src/lambda/random-dog.js
+++ src/lambda/random-dog.js
@@ -8,10 +8,11 @@ export async function handler(event, context) {
         const data = response.data
 
         const picture = await axios.get(data.message, { headers: { Accept: "application/json" } })
+        const data2 = picture.data2
 
         return {
             statusCode: 200,
-            body: picture
+            body: JSON.stringify({ msg: data2.img })
         }
 
     } catch (err) {
